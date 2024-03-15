595e96e06946fbb0b0e7b49525a21d4aaeef6e14
mmouller <cnfcnbvjattd@gmail.com>
2024-03-15 16:21:27 +0300

    Добавьте файлы проекта.

---
+++ 2.7Practice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._7Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Ivanov Ivan Ivanovich";
+            int age = 25;
+            string email = "ivanovivan@gmail.com";
+            double pointsProgramming = 4.5;
+            double pointsMath = 4.7;
+            double pointsPhysics = 3.9;
+            string pattern = "Full name: {0} \nAge: {1} \nEmail: {2} \nPoints in Programmins: {3} \nPoints in Math: {4} \nPoints in Physics: {5}";
+            Console.WriteLine(pattern,
+                              fullName,
+                              age,
+                              email,
+                              pointsProgramming,
+                              pointsMath,
+                              pointsPhysics);
+            Console.ReadKey();
+            double pointsSum = pointsProgramming + pointsMath + pointsPhysics;
+            Console.WriteLine(pointsSum);
+            Console.ReadKey();
+            double pointsArithmeticMeans = pointsSum / 3;
+            Console.WriteLine(pointsArithmeticMeans);
+            Console.ReadKey();
+
+
+        }
+    }
+}
